7f659e4ea167dc3422b1db8cce0e9170c75b2ad1
Tim David Harris <timdavidharris@gmail.com>
2021-06-24 19:04:15 -0600

    JS file

---
+++ JavaScript/variables-exercise.js
@@ -0,0 +1,17 @@
+// exercises from the bottom of this webpage: https://javascript.info/variables
+
+// Task #1
+let name, admin;
+name = "John";
+admin = name;
+console.log("Task 1 output: " + admin);
+
+// Task #2
+let homePlanet = "Earth";
+let currentUserName = "David"; // the name of a current user on a website
+console.log("Task 2 outputs: " + homePlanet + ", " + currentUser);
+
+// Taksk #3
+// we can use all CAPS for the bday variable since it is 'hard coded', but the age const will change over time
+const BIRTHDAY = '10.12.1995';
+const age = /* sudo code */  BIRTHDAY - todaysDate;
