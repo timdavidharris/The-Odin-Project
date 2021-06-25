0d9a9a405586f3aa47816f6543e8df15d7b6b12f
Tim David Harris <timdavidharris@gmail.com>
2021-06-24 19:41:47 -0600

    Working with numbers and math in JS

---
+++ JavaScript/numbers-basics.js
@@ -0,0 +1,11 @@
+// from this website: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Math
+
+// Number methods
+let lotsOfDecimal = 1.766584958675746364;
+lotsOfDecimal;
+let twoDecimalPlaces = lotsOfDecimal.toFixed(2); //rounding up to two decimal places
+console.log(twoDecimalPlaces);
+
+// exercise: https://mdn.github.io/learning-area/javascript/introduction-to-js-1/maths/editable_canvas.html
+
+// Test your skills Math: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Test_your_skills:_Math
