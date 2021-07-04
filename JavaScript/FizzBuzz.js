dae2d31576df169c0c6e2f72ef07763dda75f0e3
Tim David Harris <timdavidharris@gmail.com>
2021-07-04 13:30:40 -0600

    Sudo code and instructions as comments

---
+++ JavaScript/FizzBuzz.js
@@ -0,0 +1,20 @@
+// From the Problem Solving section of Odin Project:
+// https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/problem-solving
+
+
+// Instructions: Write a program that 
+// takes a user’s input and prints the numbers from 
+// one to the number the user entered. 
+// However, for multiples of three print Fizz instead 
+// of the number and for the multiples of five print Buzz. 
+// For numbers which are multiples of 
+// both three and five print FizzBuzz.
+
+// Step 1: Write out the sudo code first
+
+// 1) Ask the user for an input number
+// 2) loop from 1 to the input number
+// 3) output the numbers but
+// 3a) if the number can be divided by only 3, print Fizz
+// 3b) if the number can be divided by only 5, print Buzz
+// 3c) if the number can be divided by both, print FizzBuzz
