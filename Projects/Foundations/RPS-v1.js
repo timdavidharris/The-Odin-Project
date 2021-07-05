3457d40ea000fa0639125dda8b7a139e5a527d10
Tim David Harris <timdavidharris@gmail.com>
2021-07-05 13:35:40 -0600

    Preparing by writing out some basic steps in the comments

---
+++ Projects/Foundations/RPS-v1.js
@@ -0,0 +1,10 @@
+// Goal: play rock paper scissors against the computer
+// see full instructions here:
+// https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors
+// the computer will pick their weapon randomly
+// see steps below
+
+// 1) computer will pick a weapon randomly
+// 2) the user will input which weapon they want to use
+// 3) the code will analyze which weapon won
+// 3) the console log will declare the winner
