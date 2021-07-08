61059a74f68c77486c5b4599307542f9d593bbee
Tim Harris <70078987+timdavidharris@users.noreply.github.com>
2021-07-08 16:52:12 -0600

    Update RPS-v1.js
    1) trimming comments; 2) adding a function to check the user input; 3) adding a function to return the final score

--- Projects/Foundations/RPS-v1.js
+++ Projects/Foundations/RPS-v1.js
@@ -1,35 +1,18 @@
 // Goal: play rock paper scissors against the computer
 // see full instructions here:
 // https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors
-// the computer will pick their weapon randomly
-// see steps below
-
-// First) create an array with the possible options
-// 1) the user will be asked to input what weapon they want
-// 2) computer will pick a weapon randomly
-// 3) the code will analyze which weapon won
-// 3a)
-// rock > scissors
-// paper > rock
-// scissors > paper
-// if they're the same, declare a tie
-// 4) the console log will declare the winner
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return `It's a tie! You played ${playerSelection} and the computer played ${computerSelection}.`;
+        return `It's a tie!`;
     } else if ((playerSelection === 'rock' && computerSelection === 'paper') || 
                 (playerSelection === 'scissors' && computerSelection === 'rock') ||
                 (playerSelection === 'paper' && computerSelection === 'scissors')) {
         computerScore+=1;
-        return `You played ${playerSelection} and the computer played ${computerSelection}. The computer won.`;
-    } else if ((playerSelection === 'paper' && computerSelection === 'rock') ||
-                (playerSelection === 'rock' && computerSelection === 'scissors') ||
-                (playerSelection === 'scissors' && computerSelection === 'paper')) {
-        playerScore+=1;
-        return `You played ${playerSelection} and the computer played ${computerSelection}. Yay, you won.`;
+        return `The computer won.`;
     } else {
-        return `Sorry, I did not recognize "${playerSelection}".`;
+        playerScore+=1;
+        return `Yay, you won.`;
     }
   }
 
@@ -38,23 +21,42 @@ function computerPlay() {
     return choice[randomChoice];
 }
 
+function playerSelectionPrompt() {
+    let playerChoicePrompt = '';
+    playerChoicePrompt = prompt("Please select your weapon of choice; " + 
+    "rock, paper, or scissors:'")
+    if ((playerChoicePrompt === 'rock') ||
+        (playerChoicePrompt === 'paper') ||
+        (playerChoicePrompt === 'scissors')){
+    let playerSelection = playerChoicePrompt.toLowerCase();
+    return playerSelection;
+    } else {
+        alert("Sorry, I did not understand your input, " + 
+                "please try again")
+        playerSelectionPrompt();
+    }
+}
+
 function game() {
 for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt('Please select your weapon of choice; \
-                                    rock, paper, or scissors:').toLowerCase();
+    let playerSelection = playerSelectionPrompt();
     let computerSelection = computerPlay();
     let result = playRound(playerSelection, computerSelection);
     console.log(result);
     if (i === 4) {
-        if (playerScore > computerScore) {
-            console.table(`%c You win! You scored ${playerScore} and the computer scored ${computerScore}`, `background:black;color:green`);
-        } else if (computerScore > playerScore) {
-            console.table(`%c You lose! You scored ${playerScore} and the computer scored ${computerScore}`, `background:black;color:red`);
-        } else {
-            console.table(`%c You tie! You scored ${playerScore} and the computer scored ${computerScore}`, `background:black;color:yellow`);
-        }
+        return finalScoreReadout();
     }
 }
+}
+
+function finalScoreReadout() {
+    if (playerScore > computerScore) {
+        console.table(`%c You win! You scored ${playerScore} and the computer scored ${computerScore}`, `background:black;color:green`);
+    } else if (computerScore > playerScore) {
+        console.table(`%c You lose! You scored ${playerScore} and the computer scored ${computerScore}`, `background:black;color:red`);
+    } else {
+        console.table(`%c You tie! You scored ${playerScore} and the computer scored ${computerScore}`, `background:black;color:yellow`);
+    }
 }
 
 let choice = ['rock', 'paper', 'scissors'];
