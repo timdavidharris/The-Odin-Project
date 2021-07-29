0c6fafa1ff3bf88b1a1c9beabb814191d529a3d3
Tim David Harris <timdavidharris@gmail.com>
2021-07-28 19:36:49 -0600

    changing the while loop to a for loop

--- Projects/Foundations/etch-a-sketch.js
+++ Projects/Foundations/etch-a-sketch.js
@@ -1,9 +1,10 @@
 const mainContainer = document.querySelector('main');
 
-// create a webpage with a 16x16 grid of square divs -- 256 divs in total
+// create a webpage with a 16x16 grid of square divs = 256 divs in total
 function addADiv(input) {
     for (let i = 0; i < input; i++) {
         let div = document.createElement('div');
+        div.textContent = `div ${i}`;
         mainContainer.appendChild(div);
     }
 }
