3dfcd8d3fd7c2f6af4ea5e644c72fbd8eed66ac8
Tim David Harris <timdavidharris@gmail.com>
2021-07-29 19:28:18 -0600

    working on changing the color with either mouseover or mouseout events

--- Projects/Foundations/etch-a-sketch.js
+++ Projects/Foundations/etch-a-sketch.js
@@ -1,12 +1,21 @@
 const mainContainer = document.querySelector('main');
 
 // create a webpage with a 16x16 grid of square divs = 256 divs in total
-function addADiv(input) {
+function addTheDivContainer(input) {
     for (let i = 0; i < input; i++) {
         let div = document.createElement('div');
-        div.textContent = `div ${i}`;
+        div.textContent = `|||X||`;
+        div.setAttribute('id','div');
         mainContainer.appendChild(div);
     }
 }
 
-addADiv(256);
+addTheDivContainer(256);
+
+let divList = document.querySelectorAll('div');
+div.onmouseover = changeColor();
+div.onmouseout = changeColor();
+
+function changeColor() {
+    theDiv.style.cssText = 'background-color: yellow;';
+}
