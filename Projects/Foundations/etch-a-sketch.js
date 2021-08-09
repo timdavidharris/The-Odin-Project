44e210daf8595a648b0bcb61b4de5229329854de
Tim David Harris <timdavidharris@gmail.com>
2021-08-09 16:54:56 -0600

    Reorganizing how everything goes from the top of the file down. Adding some code to set the CSS dynamically. Removing functions that are unneeded

--- Projects/Foundations/etch-a-sketch.js
+++ Projects/Foundations/etch-a-sketch.js
@@ -2,36 +2,27 @@ const mainContainer = document.querySelector('main');
 const h1 = document.querySelector('h1');
 const resetBtn = document.createElement('button');
 
+addTheDivContainer(256);
+
 resetBtn.textContent = 'RESET'
 h1.appendChild(resetBtn);
 resetBtn.addEventListener('click', () => {
     clearScreen();
 });
 
-// create a webpage with a 16x16 grid of square divs = 256 divs in total
 function addTheDivContainer(input) {
     for (let i = 0; i < input; i++) {
         let div = document.createElement('div');
-        div.setAttribute('id','div');
-        div.textContent = '||||||||||||||||||';
+        div.setAttribute('class','div');
+        div.textContent = '+';
         mainContainer.appendChild(div);
     }
     changeColor();
-    setGrid((Math.sqrt(input)));
-}
-
-addTheDivContainer(256);
-
-function setGrid(userNumber) {
-    let column = userNumber
-    mainContainer.style.display = 'grid';
-    mainContainer.style.martin = '.5em .5em .5em .5em'
-    mainContainer.style.justifyItems = 'center';
-    mainContainer.style.gridTemplateColumns = `repeat(${column}, 1fr)`
+    setStyles((Math.sqrt(input)));
 }
 
 function changeColor() {
-    let divElements = document.querySelectorAll('#div');
+    let divElements = document.querySelectorAll('.div');
     divElements.forEach((item) => {
         item.addEventListener('mouseenter', (e) => {
             e.target.style.backgroundColor = 'black';
@@ -39,29 +30,40 @@ function changeColor() {
     });
 }
 
+function setStyles(userNumber) {
+    let inputNum = userNumber
+    mainContainer.style.display = 'grid';
+    mainContainer.style.margin = '.5em .5em .5em .5em'
+    mainContainer.style.justifyItems = 'center';
+    mainContainer.style.gridTemplateColumns = `repeat(${inputNum}, 1fr)`
+    if (inputNum < 38) {
+        let divElements = document.querySelectorAll('.div');
+        divElements.forEach((item) => {
+            item.style.padding = '60% 60% 60% 60%';
+        });
+    } else {
+        let divElements = document.querySelectorAll('.div');
+        divElements.forEach((item) => {
+            item.style.padding = '30% 30% 30% 30%';
+        });
+    }
+}
+
 function clearScreen() {
-    let divElements = document.querySelectorAll('#div');
+    let divElements = document.querySelectorAll('.div');
     divElements.forEach((item) => {
-            item.style.backgroundColor = 'white';
+        item.remove();
     });
     changeColor();
     newDimensions();
 }
 
 function newDimensions() {
-    let userNumber = prompt('Choose a new layout size that is 20 by 20 or less:')
-        if (userNumber > 20) {
+    let userNumber = prompt('Choose a new layout size that is 64 by 64 or less:')
+        if (userNumber > 64) {
             alert('Sorry, I didn\'t understand you input. Try again.')
             newDimensions();
         } 
     let newLayout = userNumber * userNumber;
-    clearOldLayout();
     addTheDivContainer(newLayout);
-}
-
-function clearOldLayout() {
-    let divElements = document.querySelectorAll('#div');
-    divElements.forEach((item) => {
-            item.remove();
-    });
 }
