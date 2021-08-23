b1f1119303d04d6c460f4e6f80e032e887a084bb
Tim David Harris <timdavidharris@gmail.com>
2021-08-22 20:22:59 -0600

    adding the variables for each button

---
+++ Projects/Foundations/calculator.js
@@ -0,0 +1,23 @@
+const zero = document.querySelector('#zero');
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+
+const clearButton = document.querySelector('#clear-btn');
+const divide = document.querySelector('#divide');
+const multiply = document.querySelector('#multiply');
+const minus = document.querySelector('#minus');
+const add = document.querySelector('#add');
+const equals = document.querySelector('#equals');
+const percent = document.querySelector('#percent');
+
+
+function operate() {
+
+}
