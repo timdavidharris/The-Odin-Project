95031b6a9f75e2f66fc709d001ff3258270e6962
Tim Harris <70078987+timdavidharris@users.noreply.github.com>
2021-08-27 16:21:25 -0600

    Update calculator.js
    made it able to calculate two numbers together

--- Projects/Foundations/calculator.js
+++ Projects/Foundations/calculator.js
@@ -17,28 +17,18 @@ allBtns.forEach((button) => {
         if (button.id === 'clear') {
             clearAll();
         } else if (button.id === '=') {
-            console.log('The equals sign button was pressed');
             operate(inputArray);
         } else if (isNaN(+button.id)) {
             inputArray[i] = button.id;
             display.textContent = inputArray.join('');
             i++;
-            checkArrayLength(inputArray.length);
         } else {
             inputArray[i] = +button.id;
             display.textContent = inputArray.join('');
             i++;
-            checkArrayLength(inputArray.length);
         }
     });
 });
-
-function checkArrayLength(array) {
-    arrayLength = array;    
-    if (arrayLength >=3 ) {
-        console.log('Array length is >= 3');
-    }
-}
 
 function clearAll() {
     display.textContent = 0;
@@ -50,44 +40,57 @@ function add(num1, num2) {
     let sum = num1 + num2;
     display.textContent = sum;
     console.log(sum);
+    runningTotal(sum);
 }
 
 function subtract(num1, num2) {
     let sum = num1 - num2;
     display.textContent = sum;
     console.log(sum);
+    runningTotal(sum);
 }
 
 function multiply(num1, num2) {
     let sum = num1 * num2;
     display.textContent = sum;
     console.log(sum);
+    runningTotal(sum);
 }
 
 function divide(num1, num2) {
     let sum = num1 / num2;
     display.textContent = sum;
     console.log(sum);
+    runningTotal(sum);
 }
 
 // 1) consolidate double digit numbers
 // 2) check the operator
 // 3) set up the items as num1 & num2
 function operate(inputArray) {
-    inputArray.forEach((item) => {
-        inputArray.join('');
-        switch(item) {
-            case '+':
-                operateArray.split('+');
-            case '-':
-                operateArray.split('-');
-            case '/':
-                operateArray.split('/');
-            case '*':
-                operateArray.split('*');
+    num1 = inputArray[0];
+    num2 = inputArray[2];
+    item = inputArray[1];
+    if ((inputArray[1] === 'number') ||
+        (isNaN(inputArray[0])) ||
+        (isNaN(inputArray[2]))) {
+        display.textContent = 'ERROR';
+    }
+        if (item === '+') {
+            add(num1, num2);
+        } else if (item === '-') {
+            subtract(num1, num2);
+        } else if (item === '/') {
+            divide(num1, num2);
+        } else if (item === '*') {
+            multiply(num1, num2);
         }
-    });
-    console.log(operateArray);
+}
+
+function runningTotal(sum) {
+    inputArray = [];
+    inputArray[0] = sum;
+    i = 1;
 }
 
 // array.forEach((item) => {
@@ -110,3 +113,19 @@ function operate(inputArray) {
 //         console.log(sum);
 //     }
 // });
+
+
+    // inputArray.forEach((item) => {
+    //     inputArray.join('');
+    //     switch(item) {
+    //         case '+':
+    //             operateArray.split('+');
+    //         case '-':
+    //             operateArray.split('-');
+    //         case '/':
+    //             operateArray.split('/');
+    //         case '*':
+    //             operateArray.split('*');
+    //     }
+    // });
+    // console.log(operateArray);
