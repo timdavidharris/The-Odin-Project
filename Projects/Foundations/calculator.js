187ba20d93d53963f7d35ba73064aa20eb8bb2cc
Tim David Harris <timdavidharris@gmail.com>
2021-08-27 19:20:52 -0600

    Made it so that it can calculate if the user inters a double digit number as their first number

--- Projects/Foundations/calculator.js
+++ Projects/Foundations/calculator.js
@@ -28,25 +28,25 @@ function clearAll() {
 }
 
 function add(num1, num2) {
-    let sum = num1 + num2;
+    let sum = +num1 + num2;
     display.textContent = sum;
     runningTotal(sum);
 }
 
 function subtract(num1, num2) {
-    let sum = num1 - num2;
+    let sum = +num1 - num2;
     display.textContent = sum;
     runningTotal(sum);
 }
 
 function multiply(num1, num2) {
-    let sum = num1 * num2;
+    let sum = +num1 * num2;
     display.textContent = sum;
     runningTotal(sum);
 }
 
 function divide(num1, num2) {
-    let sum = num1 / num2;
+    let sum = +num1 / num2;
     display.textContent = sum;
     runningTotal(sum);
 }
@@ -55,23 +55,29 @@ function divide(num1, num2) {
 // 2) check the operator
 // 3) set up the items as num1 & num2
 function operate(inputArray) {
-    num1 = inputArray[0];
-    num2 = inputArray[2];
-    item = inputArray[1];
-    if ((inputArray[1] === 'number') ||
+    if (isNaN(inputArray[1])) {
+        num1 = inputArray[0];
+        num2 = inputArray[2];
+        item = inputArray[1];
+            if (item === '+') {
+                add(num1, num2);
+            } else if (item === '-') {
+                subtract(num1, num2);
+            } else if (item === '/') {
+                divide(num1, num2);
+            } else if (item === '*') {
+                multiply(num1, num2);
+            }
+      } else if ((inputArray[1] === 'number') ||
         (isNaN(inputArray[0])) ||
         (isNaN(inputArray[2]))) {
-        display.textContent = 'ERROR';
-    }
-        if (item === '+') {
-            add(num1, num2);
-        } else if (item === '-') {
-            subtract(num1, num2);
-        } else if (item === '/') {
-            divide(num1, num2);
-        } else if (item === '*') {
-            multiply(num1, num2);
-        }
+        //   display.textContent = 'ERROR';
+        newFirstArrayNum = inputArray[0].toString() + inputArray[1].toString();
+        inputArray.shift();
+        inputArray.shift();
+        inputArray.unshift(+newFirstArrayNum);
+        operate(inputArray);
+  }
 }
 
 function runningTotal(sum) {
