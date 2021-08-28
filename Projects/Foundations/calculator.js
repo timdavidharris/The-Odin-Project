9d23a57c0c7c8a68a3a9704682fe7df5e7bb28f8
Tim David Harris <timdavidharris@gmail.com>
2021-08-27 18:45:08 -0600

    removing console log for the operator functions as the output is going to the user now

--- Projects/Foundations/calculator.js
+++ Projects/Foundations/calculator.js
@@ -30,28 +30,24 @@ function clearAll() {
 function add(num1, num2) {
     let sum = num1 + num2;
     display.textContent = sum;
-    console.log(sum);
     runningTotal(sum);
 }
 
 function subtract(num1, num2) {
     let sum = num1 - num2;
     display.textContent = sum;
-    console.log(sum);
     runningTotal(sum);
 }
 
 function multiply(num1, num2) {
     let sum = num1 * num2;
     display.textContent = sum;
-    console.log(sum);
     runningTotal(sum);
 }
 
 function divide(num1, num2) {
     let sum = num1 / num2;
     display.textContent = sum;
-    console.log(sum);
     runningTotal(sum);
 }
 
