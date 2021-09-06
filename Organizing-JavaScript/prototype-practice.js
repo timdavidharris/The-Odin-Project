a056693529a3c82d5505d9eb8823ab5131a2da0c
Tim David Harris <timdavidharris@gmail.com>
2021-09-06 14:12:48 -0600

    working on understanding how prototypes work

---
+++ Organizing-JavaScript/prototype-practice.js
@@ -0,0 +1,11 @@
+function Animal(type, size, diet) {
+    this.type = type 
+    this.size = size 
+    this.diet = diet 
+}
+
+const elephant = new Animal(
+    'Mammal',
+    'Large',
+    'Herbivore'
+)
