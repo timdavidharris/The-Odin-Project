fb8142840b3364df40b49550a0928516b6fe8d8c
Tim David Harris <timdavidharris@gmail.com>
2021-09-06 15:04:49 -0600

    adding more to the different functions to work on auto populating new cards with book info

--- Projects/Organizing-JS/Library-Project/library.js
+++ Projects/Organizing-JS/Library-Project/library.js
@@ -1,22 +1,31 @@
 const mainSection = document.querySelector('#book-cards')
 let myLibrary = [];
-const parableOfTheSower = new Book("Parable of the Sower", "Octavia E Butler", "345", "read");
+const parableOfTheSower = new Book();
+parableOfTheSower.title = "Parable of the Sower"
+parableOfTheSower.author = "Octavia E Butler"
+parableOfTheSower.pages = "345" 
+parableOfTheSower.read = "read"
 
 function Book(title, author, pages, read) {
     this.title = title 
     this.author = author 
     this.pages = pages 
     this.read = read 
+    addBookToLibrary(this);
+    this.bookInfo = `${this.title} by ${this.author} has ${this.pages} pages, and is currently ${read}`
 }
 
-function addBookToLibrary(newBook) {
-    myLibrary[myLibrary.length] = newBook;
+function addBookToLibrary(book) {
+    myLibrary[myLibrary.length] = book;
+    populateCards(myLibrary, book);
 }
 
-function populateCards(myLibrary) {
-    myLibrary.forEach(book => {
+function populateCards(myLibrary, newBook) {
+    myLibrary.forEach(newBook => {
         let bookCard = document.createElement('div');
         bookCard.setAttribute('class', 'book-tile');
-        bookCard.textContent = book;
+        bookCard.textContent = newBook.bookInfo;
+        mainSection.appendChild(bookCard);
     });
 }
+
