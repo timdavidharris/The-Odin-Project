c7753619d0b8bef68ad07380d6a9b9175fcc2ce8
Tim David Harris <timdavidharris@gmail.com>
2021-09-06 14:27:28 -0600

    adding a second animal and an element unique to the Animal function

--- Organizing-JavaScript/prototype-practice.js
+++ Organizing-JavaScript/prototype-practice.js
@@ -2,6 +2,7 @@ function Animal(type, size, diet) {
     this.type = type 
     this.size = size 
     this.diet = diet 
+    this.isAnimal = true;
 }
 
 const elephant = new Animal(
@@ -9,3 +10,10 @@ const elephant = new Animal(
     'Large',
     'Herbivore'
 )
+
+const tiger = {};
+tiger.type = "mammal";
+tiger.size = "medium";
+tiger.diet = "carnivore";
+
+tiger.prototype = Object.create(Animal.prototype);
