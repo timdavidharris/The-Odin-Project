a58181f6664af4192048da6396eafb1629bb00a1
Tim Harris <70078987+timdavidharris@users.noreply.github.com>
2021-09-07 16:53:05 -0600

    Update library.js
    adding 2 more books;  adding a clear function to remove the 'untitled' book that would show up first; adjusted how the code looks for giving the book readout text

--- Projects/Organizing-JS/Library-Project/library.js
+++ Projects/Organizing-JS/Library-Project/library.js
@@ -1,10 +1,23 @@
 const mainSection = document.querySelector('#book-cards')
 let myLibrary = [];
+// book 1
 const parableOfTheSower = new Book();
 parableOfTheSower.title = "Parable of the Sower"
-parableOfTheSower.author = "Octavia E Butler"
+parableOfTheSower.author = "Octavia E. Butler"
 parableOfTheSower.pages = "345" 
 parableOfTheSower.read = "read"
+// book 2
+const parableOfTheTalents = new Book();
+parableOfTheTalents.title = "Parable of the Talents"
+parableOfTheTalents.author = "Octavia E. Butler"
+parableOfTheTalents.pages = "424" 
+parableOfTheTalents.read = "read"
+// book 3
+const Dune = new Book();
+Dune.title = "Dune"
+Dune.author = "Frank Herbert"
+Dune.pages = "688"
+Dune.read = "read"
 
 function Book(title, author, pages, read) {
     this.title = title 
@@ -16,16 +29,26 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
     myLibrary[myLibrary.length] = book;
-    populateCards(book);
+    populateCards();
 }
 
 function populateCards() {
+    clearBooks();
     myLibrary.forEach(newBook => {
         let bookCard = document.createElement('div');
         bookCard.setAttribute('class', 'book-tile');
-        bookCard.textContent = `${newBook.title} by ${newBook.author} \n has ${newBook.pages} pages, \n and is currently "${newBook.read}".`
+        bookCard.textContent = `${newBook.title} by ${newBook.author}`
+                             + ` has ${newBook.pages} pages,`
+                             + ` and is currently "${newBook.read}".`;
         mainSection.appendChild(bookCard);
     });
 }
 
-populateCards(myLibrary[0]);
+function clearBooks() {
+    let cards = document.querySelectorAll('div');
+    cards.forEach((div) => {
+        div.remove();
+    });
+}
+
+populateCards(myLibrary);
