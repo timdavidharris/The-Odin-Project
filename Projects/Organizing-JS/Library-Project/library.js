ff01a5bbd6d295818798dcb1c9c4e4b787c24e46
Tim Harris <70078987+timdavidharris@users.noreply.github.com>
2021-09-10 16:16:41 -0600

    Update library.js
    Adjusting the code to make sure you can add multiple books. Writing code to listen to clicks for the remove a book button.

--- Projects/Organizing-JS/Library-Project/library.js
+++ Projects/Organizing-JS/Library-Project/library.js
@@ -2,6 +2,7 @@ const mainSection = document.querySelector('#book-cards');
 const addABookBtn = document.querySelector('#add-book-btn');
 const submitNewBookBtn = document.querySelector('#submit-btn');
 const addABookForm = document.querySelector('#book-form');
+const removeBtns = document.querySelectorAll('.remove-btn')
 let myLibrary = [];
 // book 1
 const parableOfTheSower = new Book();
@@ -9,18 +10,21 @@ parableOfTheSower.title = "Parable of the Sower"
 parableOfTheSower.author = "Octavia E. Butler"
 parableOfTheSower.pages = "345" 
 parableOfTheSower.read = "read"
+addBookToLibrary(parableOfTheSower);
 // book 2
 const parableOfTheTalents = new Book();
 parableOfTheTalents.title = "Parable of the Talents"
 parableOfTheTalents.author = "Octavia E. Butler"
 parableOfTheTalents.pages = "424" 
 parableOfTheTalents.read = "read"
+addBookToLibrary(parableOfTheTalents);
 // book 3
 const Dune = new Book();
 Dune.title = "Dune"
 Dune.author = "Frank Herbert"
 Dune.pages = "688"
 Dune.read = "read"
+addBookToLibrary(Dune);
 
 addABookBtn.addEventListener('click', () => {
     addABookForm.style.display = '';
@@ -28,6 +32,7 @@ addABookBtn.addEventListener('click', () => {
 
 submitNewBookBtn.addEventListener('click', () => {
     let newTitle = new Book();
+    addBookToLibrary(newTitle);
     newTitle.title = document.getElementById('book-title').value;
     newTitle.author = document.getElementById('book-author').value;
     newTitle.pages = document.getElementById('book-pages').value;
@@ -35,12 +40,17 @@ submitNewBookBtn.addEventListener('click', () => {
     populateCards(myLibrary);
 });
 
+removeBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log('you clicked a "remove" button');
+    });
+});
+
 function Book(title, author, pages, read) {
     this.title = title 
     this.author = author 
     this.pages = pages 
     this.read = read 
-    addBookToLibrary(this)
 }
 
 function addBookToLibrary(book) {
@@ -57,7 +67,11 @@ function populateCards() {
         bookCard.textContent = `${newBook.title} by ${newBook.author}`
                              + ` has ${newBook.pages} pages,`
                              + ` and is currently "${newBook.read}".`;
+        let removeBookBtn = document.createElement('button');
+        removeBookBtn.setAttribute('class', 'remove-btn');
+        removeBookBtn.textContent = 'Remove';
         mainSection.appendChild(bookCard);
+        bookCard.appendChild(removeBookBtn);
     });
 }
 
