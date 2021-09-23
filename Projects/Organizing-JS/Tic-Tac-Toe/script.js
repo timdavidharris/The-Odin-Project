2b5c976681469f64f01f9e0fed8f22dc2cee105d
Tim David Harris <timdavidharris@gmail.com>
2021-09-23 16:24:58 -0600

    adding a data attribute for the styles to use

--- Projects/Organizing-JS/Tic-Tac-Toe/script.js
+++ Projects/Organizing-JS/Tic-Tac-Toe/script.js
@@ -4,7 +4,8 @@ const makeGameBoard = (() => {
     i = 0;
     while (i < 9) {
         let newDiv = document.createElement('div');
-        newDiv.setAttribute('class', 'game-square');
+        newDiv.setAttribute('class', `game-square`);
+        newDiv.setAttribute('data', `data-square-${i + 1}`)
         newDiv.textContent = "X";
         gameBoardSection.appendChild(newDiv);
         i++;
