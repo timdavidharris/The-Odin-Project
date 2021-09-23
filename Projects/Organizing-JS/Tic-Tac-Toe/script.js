b61beabf234e042a2723e8e1321c7b0f9b2babc8
Tim David Harris <timdavidharris@gmail.com>
2021-09-23 16:44:49 -0600

    added a module to keep tabs on players. Added a module to let the user click on the game squares to switch them from 'o' to 'x'

--- Projects/Organizing-JS/Tic-Tac-Toe/script.js
+++ Projects/Organizing-JS/Tic-Tac-Toe/script.js
@@ -6,11 +6,45 @@ const makeGameBoard = (() => {
         let newDiv = document.createElement('div');
         newDiv.setAttribute('class', `game-square`);
         newDiv.setAttribute('data', `data-square-${i + 1}`)
-        newDiv.textContent = "X";
+        newDiv.textContent = "";
         gameBoardSection.appendChild(newDiv);
         i++;
-        console.log(i);
     }   
-
 })();
 
+const playARound = (() => {
+    let turnCounter = 0;
+    const playerX = (turn, marker) => {
+        if (turnCounter % 2 === 0){
+        this.turn = null; 
+        } else {
+            this.turn = true;
+        }
+        this.marker = "x";
+        turnCounter++;
+        return this.turn, this.marker;
+    };
+    const playerO = (turn, marker) => {
+        if (turnCounter % 2 !== 0){
+        this.turn = true;
+        } else {
+            this.turn = null;
+        }
+        turnCounter++;
+        this.marker = "o";
+        return this.turn, this.marker;
+    };
+})();
+
+const clickOnGameSquares = (() => {
+    let gameSquares = document.querySelectorAll('.game-square');
+    gameSquares.forEach((div) => {
+        div.addEventListener('click', () => {
+            if (div.textContent === "o") {
+            div.textContent = "x";
+            } else {
+                div.textContent = "o"
+            }
+        });
+    });
+})();
