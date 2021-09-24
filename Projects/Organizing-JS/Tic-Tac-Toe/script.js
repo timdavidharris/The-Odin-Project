a46b8293e116e87f6fa1d580422918898c2dff60
Tim Harris <70078987+timdavidharris@users.noreply.github.com>
2021-09-24 16:22:51 -0600

    Update script.js
    Building out text that lets the user know who's turn it is. Keeping track of if a click should initiate an X or an O.

--- Projects/Organizing-JS/Tic-Tac-Toe/script.js
+++ Projects/Organizing-JS/Tic-Tac-Toe/script.js
@@ -1,4 +1,8 @@
 const gameBoardSection = document.getElementById('Game-Board-Section');
+const player1Turn = document.getElementById('player-1-turn');
+const player2Turn = document.getElementById('player-2-turn');
+player1Turn.textContent = "Player 1 (X's): GO";
+player2Turn.textContent = "Player 2 (O's): WAIT";
 
 const makeGameBoard = (() => {
     i = 0;
@@ -12,39 +16,56 @@ const makeGameBoard = (() => {
     }   
 })();
 
-const playARound = (() => {
-    let turnCounter = 0;
-    const playerX = (turn, marker) => {
-        if (turnCounter % 2 === 0){
-        this.turn = null; 
-        } else {
-            this.turn = true;
-        }
-        this.marker = "x";
-        turnCounter++;
-        return this.turn, this.marker;
-    };
-    const playerO = (turn, marker) => {
-        if (turnCounter % 2 !== 0){
-        this.turn = true;
-        } else {
-            this.turn = null;
-        }
-        turnCounter++;
-        this.marker = "o";
-        return this.turn, this.marker;
-    };
-})();
-
-const clickOnGameSquares = (() => {
+const onClickGameSquares = (() => {
     let gameSquares = document.querySelectorAll('.game-square');
+    let turnCounter = 0;
     gameSquares.forEach((div) => {
         div.addEventListener('click', () => {
-            if (div.textContent === "o") {
-            div.textContent = "x";
+            if (turnCounter % 2 === 0) {
+                div.textContent = "x";
             } else {
                 div.textContent = "o"
             }
+            turnCounter++;
+            updateTurnText();
+            console.log(turnCounter)
         });
     });
+    function updateTurnText() {
+        if (player1Turn.textContent === "Player 1 (X's): GO") {
+            player1Turn.textContent = "Player 1 (O's): WAIT";
+            player2Turn.textContent = "Player 2 (X's): GO";
+        } else {
+            player1Turn.textContent = "Player 1 (X's): GO";
+            player2Turn.textContent = "Player 2 (O's): WAIT";
+        }
+    };    
 })();
+
+
+// ----------------------------------------------------------------------
+// const playARound = (playerX, playerO) => {
+//     let turnCounter = 0;
+//     playerX = (turn, marker) => {
+//         console.log(playerX);
+//         if (turnCounter % 2 === 0){
+//         this.turn = null; 
+//         } else {
+//             this.turn = true;
+//         }
+//         this.marker = "x";
+//         turnCounter++;
+//         return this.turn, this.marker;
+//     };
+//     playerO = (turn, marker) => {
+//         console.log(playerO);
+//         if (turnCounter % 2 !== 0){
+//         this.turn = true;
+//         } else {
+//             this.turn = null;
+//         }
+//         turnCounter++;
+//         this.marker = "o";
+//         return this.turn, this.marker;
+//     };
+// };
