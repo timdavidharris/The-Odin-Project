3885d8d539dd9d88daee117b73fbc8ca13c67215
Tim David Harris <timdavidharris@gmail.com>
2021-09-25 16:23:43 -0600

    adding logic to check for win conditions

--- Projects/Organizing-JS/Tic-Tac-Toe/script.js
+++ Projects/Organizing-JS/Tic-Tac-Toe/script.js
@@ -9,7 +9,7 @@ const makeGameBoard = (() => {
     while (i < 9) {
         let newDiv = document.createElement('div');
         newDiv.setAttribute('class', `game-square`);
-        newDiv.setAttribute('data', `data-${i + 1}`);
+        newDiv.setAttribute('data-square', `${i + 1}`);
         newDiv.textContent = "";
         gameBoardSection.appendChild(newDiv);
         i++;
@@ -38,20 +38,51 @@ const onClickGameSquares = (() => {
     });
     function updateTurnText() {
         if (player1Turn.textContent === "Player 1 (X's): GO") {
-            player1Turn.textContent = "Player 1 (O's): WAIT";
-            player2Turn.textContent = "Player 2 (X's): GO";
+            player1Turn.textContent = "Player 1 (X's): WAIT";
+            player2Turn.textContent = "Player 2 (O's): GO";
         } else {
             player1Turn.textContent = "Player 1 (X's): GO";
             player2Turn.textContent = "Player 2 (O's): WAIT";
         }
     };   
     function checkWinCondition() {
-        let square = document.querySelectorAll('.game-square');
-        square.forEach(() => {
-            // console.log(square.dataset.square);
-        });
-    } 
+        let gameBoxes = document.querySelectorAll('.game-square');
+        let gameBox1 = gameBoxes.item(0).textContent;
+        let gameBox2 = gameBoxes.item(1).textContent;
+        let gameBox3= gameBoxes.item(2).textContent;
+        let gameBox4 = gameBoxes.item(3).textContent;
+        let gameBox5 = gameBoxes.item(4).textContent;
+        let gameBox6 = gameBoxes.item(5).textContent;
+        let gameBox7 = gameBoxes.item(6).textContent;
+        let gameBox8 = gameBoxes.item(7).textContent;
+        let gameBox9 = gameBoxes.item(8).textContent;
+        // Middle row win condition
+        if ((gameBox4 === gameBox5) && 
+              (gameBox4 === gameBox6)) {
+                if (gameBox4 === 'x') {
+                    console.log('Player 1 (X\'s) Wins!')
+                } else if (gameBox4 === 'o') {
+                    console.log('Player 2 (O\'s) Wins')
+                }
+        } else if ((gameBox7 === gameBox8) && 
+                   (gameBox7 === gameBox9)) {
+                if (gameBox7 === 'x') {
+                    console.log('Player 1 (X\'s) Wins!')
+                } else if (gameBox7 === 'o') {
+                    console.log('Player 2 (O\'s) Wins') 
+                }
+        // top row win condition
+        } else if ((gameBox1 === gameBox2) && 
+                    (gameBox1 === gameBox3)) {
+                if (gameBox1 === 'x') {
+                    console.log('Player 1 (X\'s) Wins!')
+                } else if (gameBox1 === 'o') {
+                    console.log('Player 2 (O\'s) Wins')
+            }
+        }
+    }
 })();
+
 
 
 
