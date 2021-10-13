f74fa56d035f9577b0f833ebdb60824c6bef3f3c
Tim David Harris <timdavidharris@gmail.com>
2021-10-13 16:43:04 -0600

    Adding a quote under the 'about' section

--- Projects/Organizing-JS/Restaurant-Page/src/home.js
+++ Projects/Organizing-JS/Restaurant-Page/src/home.js
@@ -1,15 +1,20 @@
+import { first } from "lodash";
+
 export default function makeHomePage(contentDivInput) {
     let contentDiv = contentDivInput;
     console.log('makeHomePage() is working');
     // creating elements
     const homeImage = document.createElement('img');
     const infoDiv = document.createElement('div');
+    const reviewsDiv = document.createElement('div');
     homeImage.src = '../assets/images/burger-overhead-shot.jpg';
     // appending elements
     contentDiv.append(homeImage);
     contentDiv.append(infoDiv);
+    contentDiv.append(reviewsDiv);
     // calling functions
     infoDivText(infoDiv);
+    reviewsDivText(reviewsDiv);
 }
 
 function infoDivText(input) {
@@ -18,4 +23,11 @@ function infoDivText(input) {
         'We are a local, family-owned business that focuses on one thing: ' +
         'making the best damn burger you\'ve ever had.' +
         'Please place an order online or come in person to see for yourself!'
+}
+
+function reviewsDivText(input) {
+    let reviewsDiv = input;
+    const firstQuote = document.createElement('q');
+    firstQuote.textContent = 'These burgers literally blew my mind. Don\'t miss an opportunity to get your mind blown';
+    reviewsDiv.append(firstQuote);
 }
