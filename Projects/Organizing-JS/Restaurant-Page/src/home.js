19a86b9f74042576a654db2ff29f12eb3d1891f7
Tim David Harris <timdavidharris@gmail.com>
2021-10-13 19:44:44 -0600

    adding some padding to everything and adding a second quote

--- Projects/Organizing-JS/Restaurant-Page/src/home.js
+++ Projects/Organizing-JS/Restaurant-Page/src/home.js
@@ -21,13 +21,22 @@ function infoDivText(input) {
     let infoDiv = input;
     infoDiv.textContent = 'Welcome to Tim\'s Burgers! ' +
         'We are a local, family-owned business that focuses on one thing: ' +
-        'making the best damn burger you\'ve ever had.' +
+        'making the best damn burger you\'ve ever had. ' +
         'Please place an order online or come in person to see for yourself!'
 }
 
 function reviewsDivText(input) {
     let reviewsDiv = input;
     const firstQuote = document.createElement('q');
+    const secondQuote = document.createElement('q');
+    const firstQuotePerson = document.createElement('p');
+    const secondQuotePerson = document.createElement('p');
     firstQuote.textContent = 'These burgers literally blew my mind. Don\'t miss an opportunity to get your mind blown';
+    firstQuotePerson.textContent = '~ Simon from Uptown'
+    secondQuote.textContent = 'I thought that I\'d had a good burger before I tried Tim\'s. I was wrong.'
+    secondQuotePerson.textContent = '~ Guy Fieri'
     reviewsDiv.append(firstQuote);
+    reviewsDiv.append(firstQuotePerson);
+    reviewsDiv.append(secondQuote);
+    reviewsDiv.append(secondQuotePerson);
 }
