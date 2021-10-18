cb090caa14799b3542bccc5a0a473c7c268e6d88
Tim David Harris <timdavidharris@gmail.com>
2021-10-18 15:59:28 -0600

    adding sides to the menu page

--- Projects/Organizing-JS/Restaurant-Page/src/menu.js
+++ Projects/Organizing-JS/Restaurant-Page/src/menu.js
@@ -10,24 +10,34 @@ export default function menuPage(contentDivInput) {
         contentDiv.append(menuDiv);
         menuDiv.setAttribute('class', 'page-div');
         const menuImage = document.createElement('img');
-        const theMenu = document.createElement('div');
-        theMenu.setAttribute('style', 'white-space: pre;');
+        const burgers = document.createElement('p');
+        const sides = document.createElement('p');
+        burgers.setAttribute('style', 'white-space: pre;');
+        sides.setAttribute('style', 'white-space: pre;');
         menuImage.setAttribute('class', 'menu');
         menuImage.src = '../assets/images/burger-on-a-tray.jpg';
         menuDiv.append(menuImage);
-        menuDivText(theMenu);
-        menuDiv.append(theMenu);
+        menuDivText(burgers, sides);
+        menuDiv.append(burgers);
+        menuDiv.append(sides);
         i++;
     }
     menuDiv.style.display = 'contents';
 }
 
-function menuDivText(input) {
-    let theMenu = input;
-    theMenu.textContent = "BURGERS\r\n";
-    theMenu.textContent += "PVT Classic: $10\r\n";
-    theMenu.textContent += "SGT Cheese: $10\r\n";
-    theMenu.textContent += "CPT Bacon: $10\r\n";
-    theMenu.textContent += "MAJ Mushroom: $10\r\n";
-    theMenu.textContent += "GEN Ultimate: $18";
+function menuDivText(theBurgers, theSides) {
+    let burgers = theBurgers;
+    burgers.textContent = "BURGERS\r\n";
+    burgers.textContent += "PVT Classic: $10\r\n";
+    burgers.textContent += "SGT Cheese: $10\r\n";
+    burgers.textContent += "CPT Bacon: $10\r\n";
+    burgers.textContent += "MAJ Mushroom: $10\r\n";
+    burgers.textContent += "GEN Ultimate: $18";
+    let sides = theSides;
+    sides.textContent = "SIDES\r\n";
+    sides.textContent += "Small Fries: $3\r\n";
+    sides.textContent += "Medium Fries: $5\r\n";
+    sides.textContent += "Large Fries: $7\r\n";
+    sides.textContent += "Loaded Fries (Medium): $8\r\n";
+    sides.textContent += "Sweet Potato Fries: + $1.50\r\n";
 }
