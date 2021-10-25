cfbd961361b511f3fc01ef417ce5707baffaf7fa
Tim David Harris <timdavidharris@gmail.com>
2021-10-25 15:39:10 -0600

    Adding functions to give more clarity for the page-switcher JS file

--- Projects/Organizing-JS/Restaurant-Page/src/page-switcher.js
+++ Projects/Organizing-JS/Restaurant-Page/src/page-switcher.js
@@ -6,16 +6,20 @@ export default function pageSwitcher(home, menu, contact) {
     let menuLink = document.querySelector('#nav-MENU');
     let contactLink = document.querySelector('#nav-CONTACT');
     const pageLinks = document.querySelectorAll('.nav-link');
-    homeLink.style.color = '#5F9EA0';
-    homeDiv.style.display = 'contents';
+    (function setHomePage() {
+        homeLink.style.color = '#5F9EA0';
+        homeDiv.style.display = 'contents';
+    })();
     pageLinks.forEach((link) => {
         link.addEventListener('click', () => {
-            homeDiv.style.display = 'none';
-            menuDiv.style.display = 'none';
-            contactDiv.style.display = 'none';
-            homeLink.style.color = 'white';
-            menuLink.style.color = 'white';
-            contactLink.style.color = 'white';
+            (function resetStyles() {
+                homeDiv.style.display = 'none';
+                menuDiv.style.display = 'none';
+                contactDiv.style.display = 'none';
+                homeLink.style.color = 'white';
+                menuLink.style.color = 'white';
+                contactLink.style.color = 'white';
+            })();
             if (link.textContent === "HOME") {
                 homeDiv.style.display = 'contents';
                 homeLink.style.color = '#5F9EA0';
