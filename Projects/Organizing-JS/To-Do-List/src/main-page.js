aded2639d570558db25a20e8c2959a3c7889a65a
Tim David Harris <timdavidharris@gmail.com>
2021-10-30 17:02:01 -0600

    adding buttons for adding or deleting new to dos

--- Projects/Organizing-JS/To-Do-List/src/main-page.js
+++ Projects/Organizing-JS/To-Do-List/src/main-page.js
@@ -10,9 +10,23 @@ function mainPage() {
     mainPageLI.setAttribute('class', 'list-group-item');
     let mainPageInput = document.createElement('input');
     mainPageInput.setAttribute('class', 'form-check-input me-1');
+    let mainPageButton = document.createElement('button');
+    mainPageButton.setAttribute('class', 'btn btn-sm btn-outline-danger justify-content-end');
+    mainPageButton.setAttribute('type', 'button');
     mainPageInput.setAttribute('type', 'checkbox');
+    mainPageButton.textContent = "delete";
     toDoDiv.append(mainPageUL);
     mainPageUL.append(mainPageLI); 
     mainPageLI.append(mainPageInput);
-    mainPageLI.append("test item");
+    mainPageLI.append("*example*");
+    mainPageLI.append(mainPageButton);
+    // 
+    let mainPageLI2 = document.createElement('li');
+    mainPageLI2.setAttribute('class', 'list-group-item');
+    let mainPageButton2 = document.createElement('button');
+    mainPageButton2.textContent = "Add New";
+    mainPageButton2.setAttribute('class', 'btn btn-sm btn-outline-success');
+    mainPageButton2.setAttribute('type', 'button');
+    mainPageUL.append(mainPageLI2); 
+    mainPageLI2.append(mainPageButton2);
 }
