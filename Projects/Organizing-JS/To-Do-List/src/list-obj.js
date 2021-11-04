ba8f6811121e24698883300da3815b05b4f310c9
Tim David Harris <timdavidharris@gmail.com>
2021-11-04 15:15:25 -0600

    Moving the contents from the 'tab-obj' file into the 'list-obj' file

--- Projects/Organizing-JS/To-Do-List/src/list-obj.js
+++ Projects/Organizing-JS/To-Do-List/src/list-obj.js
@@ -1,7 +1,17 @@
 import { createToDoTab } from './tab-obj';
-export { list };
+export { list, createToDoTab };
+
+let toDoParentDiv = document.querySelector('#to-do-items');
+let tabNum = 1;
 
 function list(name) {
     this.name = name
     createToDoTab();
+}
+
+function createToDoTab() {
+    let newTab = document.createElement('div');
+    newTab.setAttribute('id', `to-do-${tabNum}`);
+    toDoParentDiv.append(newTab);
+    tabNum++;
 }
