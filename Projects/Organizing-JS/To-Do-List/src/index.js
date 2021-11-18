057a48011de68f6e117d0d15682825d798d193f9
Tim David Harris <timdavidharris@gmail.com>
2021-11-18 16:12:37 -0700

    Removing the 'mod' as part of the import as it is unneeded

--- Projects/Organizing-JS/To-Do-List/src/index.js
+++ Projects/Organizing-JS/To-Do-List/src/index.js
@@ -1,16 +1,16 @@
 import _ from "lodash";
 import "./bootstrap.min.css";
 import "./style.css";
-import * as toDoMod from "./to-do-obj";
-import * as listMod from "./list-obj";
+import * as toDo from "./to-do-obj";
+import * as list from "./list-obj";
 // import * as storageMod from "./local-storage";
-listMod.addListDiv();
-listMod.addListObj();
-listMod.toggleListInputDisplay();
-toDoMod.toggleToDoInputDisplay();
-toDoMod.addToDoObj();
-toDoMod.clearToDoLocalStorage();
-toDoMod.setItemArrayVarBasedOnStorage();
+list.addListDiv();
+list.addListObj();
+list.toggleListInputDisplay();
+toDo.toggleToDoInputDisplay();
+toDo.addToDoObj();
+toDo.clearToDoLocalStorage();
+toDo.setItemArrayVarBasedOnStorage();
 
 export function getTodaysDate() {
     let today = new Date();
