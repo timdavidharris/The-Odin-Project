083c28c4fe661ed590157ce01db4a6ac7643a245
Tim David Harris <timdavidharris@gmail.com>
2021-12-16 21:27:13 -0700

    I added a catch method to catch any errors and give the user an error message. I added a h2 to add the error message to.

--- Async-JS-and-APIs/giphy-gif-search.js
+++ Async-JS-and-APIs/giphy-gif-search.js
@@ -1,6 +1,7 @@
 const img = document.querySelector("img");
 const refreshBtn = document.querySelector("#refresh-btn");
 const searchBtn = document.querySelector("#search-btn");
+const errorMessage = document.querySelector("#error-message");
 let searchInput = document.querySelector("#search-input");
 
 function fetchCatGif() {
@@ -21,15 +22,22 @@ function refresh() {
 
 function search() {
     searchBtn.addEventListener("click", () => {
-        console.log(searchInput.value);
+    clearErrorMessage();
         fetch(`https://api.giphy.com/v1/gifs/translate?api_key=jrGcC2NtaNw49N2MXYDQWT9tIEy5ZMom&s=${searchInput.value}`, {mode: "cors"})
         .then(function(response) {
             return response.json();
         })
         .then(function(response) {
             img.src = response.data.images.original.url;
+        })
+        .catch(function(response) {
+            errorMessage.textContent = response;
+        });
     });
-    });
+}
+
+function clearErrorMessage() {
+    errorMessage.textContent = "";
 }
 
 fetchCatGif();
