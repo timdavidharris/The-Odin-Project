24ef62e4b62059d7f85e896da5f24f662f7b9319
Tim David Harris <timdavidharris@gmail.com>
2021-12-18 16:18:18 -0700

    Starting on code to practice async and await instead of promise syntax.

---
+++ Async-JS-and-APIs/async-gif-practice.js
@@ -0,0 +1,61 @@
+const img = document.querySelector("img");
+const refreshBtn = document.querySelector("#refresh-btn");
+const searchBtn = document.querySelector("#search-btn");
+const errorMessage = document.querySelector("#error-message");
+const div = document.querySelector("div");
+const h1 = document.querySelector("h1");
+const input = document.querySelector("input");
+const buttons = document.querySelectorAll("button");
+let searchInput = document.querySelector("#search-input");
+
+(function setStyles() {
+    div.style.display = "flex";
+    div.style.flexDirection = "column";
+    div.style.textAlign = "center";
+    div.style.padding = ".5em 25em .5em 25em";
+    h1.style.margin = ".5em .5em .5em .5em";
+    input.style.margin = ".5em .5em .5em .5em";
+    buttons.forEach((btn) => {
+        btn.style.margin = ".5em .5em .5em .5em";
+    });
+})();
+
+function fetchCatGif() {
+    fetch("https://api.giphy.com/v1/gifs/translate?api_key=jrGcC2NtaNw49N2MXYDQWT9tIEy5ZMom&s=cats", {mode: "cors"})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            img.src = response.data.images.original.url;
+    });
+}
+
+function refresh() {
+    refreshBtn.addEventListener("click", () => {
+        return fetchCatGif();
+    });
+}
+
+function search() {
+    searchBtn.addEventListener("click", () => {
+    clearErrorMessage();
+        fetch(`https://api.giphy.com/v1/gifs/translate?api_key=jrGcC2NtaNw49N2MXYDQWT9tIEy5ZMom&s=${searchInput.value}`, {mode: "cors"})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            img.src = response.data.images.original.url;
+        })
+        .catch(function(response) {
+            errorMessage.textContent = response;
+        });
+    });
+}
+
+function clearErrorMessage() {
+    errorMessage.textContent = "";
+}
+
+fetchCatGif();
+refresh();
+search();
