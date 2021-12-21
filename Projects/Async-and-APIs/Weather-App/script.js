a6305165372ca63c7b29fc6a9fe737a8c0dfa6a6
Tim David Harris <timdavidharris@gmail.com>
2021-12-20 20:58:27 -0700

    Calling different aspects of the weather in a second fn

--- Projects/Async-and-APIs/Weather-App/script.js
+++ Projects/Async-and-APIs/Weather-App/script.js
@@ -1,11 +1,21 @@
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 let apiKey = "112eaceaf05e5ca75e1692cec5d4a11e";
 let cityName = "denver";
+let unitType = "imperial";
 
 async function getWeather() {
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`, {mode: "cors"});
-    let cityWeather = await response.json();
-    console.log(cityWeather);
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${unitType}&appid=${apiKey}`, {mode: "cors"});
+    let weatherJSON = await response.json();
+    return displayWeatherInfo(weatherJSON);
+}
+
+function displayWeatherInfo(weatherJSON) {
+    console.log(weatherJSON.name);
+    console.log(weatherJSON.main.temp);
+    console.log(weatherJSON.main.temp_min);
+    console.log(weatherJSON.main.temp_max);
+    console.log(weatherJSON);
+    console.log(weatherJSON.weather[0].description);
 }
 
 getWeather();
