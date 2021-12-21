b40e1ffe1fdb4b78ffb08287a8c87e1894bb7292
Tim David Harris <timdavidharris@gmail.com>
2021-12-20 21:06:04 -0700

    set up getWeather fn to take a dynamic city name

--- Projects/Async-and-APIs/Weather-App/script.js
+++ Projects/Async-and-APIs/Weather-App/script.js
@@ -2,8 +2,15 @@
 let apiKey = "112eaceaf05e5ca75e1692cec5d4a11e";
 let cityName = "denver";
 let unitType = "imperial";
+const cityInput = document.querySelector("#city-input");
+const searchButton = document.querySelector("#city-search-btn");
 
-async function getWeather() {
+searchButton.addEventListener("click", () => {
+    cityName = cityInput.value;
+    getWeather(cityName);
+});
+
+async function getWeather(cityName) {
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${unitType}&appid=${apiKey}`, {mode: "cors"});
     let weatherJSON = await response.json();
     return displayWeatherInfo(weatherJSON);
@@ -18,4 +25,4 @@ function displayWeatherInfo(weatherJSON) {
     console.log(weatherJSON.weather[0].description);
 }
 
-getWeather();
+getWeather(cityName);
