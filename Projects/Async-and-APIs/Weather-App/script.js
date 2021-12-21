e976c1bdb37a929bb18a52f4ab56af43236cc5bb
Tim David Harris <timdavidharris@gmail.com>
2021-12-20 20:41:11 -0700

    adding "https://" to the API call to get it to work

--- Projects/Async-and-APIs/Weather-App/script.js
+++ Projects/Async-and-APIs/Weather-App/script.js
@@ -1,10 +1,11 @@
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 let apiKey = "112eaceaf05e5ca75e1692cec5d4a11e";
-let cityName = "London";
+let cityName = "denver";
 
 async function getWeather() {
-    let response = await fetch(`api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`, {mode: "cors"});
-    return console.log(response);
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`, {mode: "cors"});
+    let cityWeather = await response.json();
+    console.log(cityWeather);
 }
 
 getWeather();
