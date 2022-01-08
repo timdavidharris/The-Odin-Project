2a796ea5aab32de5e44a690f68dbe09949179d4f
Tim David Harris <timdavidharris@gmail.com>
2022-01-07 20:16:42 -0700

    Corrected winner logic

--- React-JS/my-test-app/src/index.js
+++ React-JS/my-test-app/src/index.js
@@ -21,6 +21,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
       squares: squares,
@@ -40,9 +43,9 @@ class Board extends React.Component {
         const winner = calculateWinner(this.state.squares);
         let status;
         if (winner) {
-          status = "winner: " + winner;
+          status = "Winner: " + winner;
         } else {
-          status = "Nest player: " + (this.state.xIsNext ? "X" : "O");
+          status = "Next player: " + (this.state.xIsNext ? "X" : "O");
         }
 
         return (
@@ -84,6 +87,13 @@ render() {
 }
 }
 
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -97,16 +107,9 @@ function calculateWinner(squares) {
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (squares[a] && squares[b] === squares[b] && squares[a] === squares[c]) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
   return null;
 }
-
-// ========================================
-
-ReactDOM.render(
-<Game />,
-document.getElementById('root')
-);
