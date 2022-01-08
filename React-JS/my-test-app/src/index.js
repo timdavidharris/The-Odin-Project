d5da6e9b05505d8bfe95a8f7ba85ff5dfe9da019
Tim David Harris <timdavidharris@gmail.com>
2022-01-07 19:42:46 -0700

    onClick to Square tag

--- React-JS/my-test-app/src/index.js
+++ React-JS/my-test-app/src/index.js
@@ -26,10 +26,15 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-    }
+    };
   }
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+        <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.candleClick(i)}
+        />
+      );
     }
 
     render() {
