bf15a8213ac25a9d295d38cbedf1f80a14adcbe7
Tim David Harris <timdavidharris@gmail.com>
2022-01-09 19:18:54 -0700

    catching input value

--- React-JS/task-app/src/App.js
+++ React-JS/task-app/src/App.js
@@ -1,12 +1,39 @@
 // Responsible for handling the input field with logic
-function App() {
-  return (
-    <div className="input">
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleADD = this.handleADD.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleADD(event) {
+    console.log(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return(
+      <div className="title-and-input">
       <h1>Task List</h1>
-      <input></input>
-      <button>ADD</button>
-    </div>
-  );
+        <form onSubmit={this.handleADD}>
+          <label>
+            Type in a New Task: 
+            <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+          </label>
+          <input type="submit" value="ADD"></input>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
