d3e79f253f8a163bea01f18d23ddf2fa549d4ead
Tim David Harris <timdavidharris@gmail.com>
2022-01-09 19:41:51 -0700

    Preparing to process input data

--- React-JS/task-app/src/components/Overview.js
+++ React-JS/task-app/src/components/Overview.js
@@ -1,11 +1,16 @@
 // Responsible for rendering tasks
+// reference this video https://www.youtube.com/watch?v=GIU8ekYndKw
+// for how to use map
 import React from 'react';
 
 class Overview extends React.Component {
     render() {
         return (
             <div>
-                <h2>**List will populate here**</h2>
+                <h3>Tasks Below</h3>
+                <ul>
+                    {this.props.state.input}
+                </ul>
             </div>
         );
     }
