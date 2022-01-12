41663f4fb09440f1370c2f2c3c2ed08475ac77d8
Tim David Harris <timdavidharris@gmail.com>
2022-01-11 19:26:32 -0700

    Restarting with TOP instructions

--- React-JS/task-app/src/App.js
+++ React-JS/task-app/src/App.js
@@ -1,43 +1,32 @@
 // Responsible for handling the input field with logic
-import React from 'react';
-import Overview from "./components/Overview"
+import React, { Component } from 'react';
 
-let inputArray = [];
+class App extends Component {
+  constructor() {
+    super();
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
     this.state = {
-      list: '',
+      task: { text: ""},
+      tasks: [],
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleADD = this.handleADD.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleADD(event) {
-    inputArray.push(this.state.value);
-    console.log(inputArray);
-    event.preventDefault();
   }
 
   render() {
-    return(
-      <div className="title-and-input">
-      <h1>Task List</h1>
-        <form onSubmit={this.handleADD}>
-          <label>
-            Type in a New Task: 
-            <input type="text" value={this.state.value} onChange={this.handleChange}></input>
-          </label>
-          <input type="submit" value="ADD"></input>
+
+    const {task, tasks } = this.state;
+
+    return (
+      <div>
+        <form>
+          <label htmlFor="taskInput">Enter task</label>
+          <input type="text" id="taskInput"/>
+          <button type="submit">
+            Add Task
+          </button>
         </form>
-        <Overview array={inputArray}/>
       </div>
     );
+    }
   }
 }
 
--- React-JS/task-app/src/components/Overview.js
+++ React-JS/task-app/src/components/Overview.js
@@ -1,19 +1,3 @@
 // Responsible for rendering tasks
 // reference this video https://www.youtube.com/watch?v=GIU8ekYndKw
 // for how to use map
-import React from 'react';
-
-class Overview extends React.Component {
-    render() {
-        return (
-            <div>
-                <h3>Tasks Below</h3>
-                <ul>
-                {inputArray}
-                </ul>
-            </div>
-        );
-    }
-}
-
-export default Overview;
