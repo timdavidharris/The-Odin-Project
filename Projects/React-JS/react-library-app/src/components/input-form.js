6dc43a9de52ee7fddc2ff9f4e36482a95f0fb650
Tim David Harris <timdavidharris@gmail.com>
2022-01-19 20:17:45 -0700

    Set up with Hooks; still not working

--- Projects/React-JS/react-library-app/src/components/input-form.js
+++ Projects/React-JS/react-library-app/src/components/input-form.js
@@ -1,52 +1,66 @@
 import React, { useState } from 'react';
-import BookCard from './book-tiles';
+// import BookCard from './book-tiles';
+import uniqid from 'uniqid';
 
-class Form extends Component {
-
-
-    render() {
-
-        return (
-            <div id='input-form-div'>
-                <BookCard libraryArray={library}/>
-                <form onSubmit={this.onSubmitBook}>
-                    <label>
-                        Book Title
-                    <input 
-                        name="title_input"
-                        type="text"
-                        value={}
-                        onChange={this.handleChange}
-                        required
-                    />
-                    </label>
-                    <label>
-                        Author
-                    <input
-                        name="author_input"
-                        type="text"
-                        value={}
-                        onChange={this.handleChange}
-                        required
-                    />
-                    </label>
-                    <label>
-                        Number of Pages
-                    <input
-                        name="pages_input"
-                        type="number"
-                        value={}
-                        onChange={this.handleChange}
-                        required
-                    />
-                    </label>
-                    <button type='submit' value="Submit">
-                        ADD
-                    </button>
-                </form>
-            </div>
-        )
-    }
+const initialValues = {
+    book: {
+        title: '',
+        author: '',
+        pages: '',
+        id: uniqid()
+    },
+    library: [],
 }
 
-export default Form;
+export default function Form() {
+    const [values, setValues] = useState(initialValues);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setValues({
+            ...values,
+            [name]: value,
+        })
+    }
+
+    return (
+        <div id='input-form-div'>
+            {/* <BookCard libraryArray={library}/> */}
+            <form>
+                <label>
+                    Book Title
+                <input 
+                    name="title_input"
+                    type="text"
+                    value={values.book.title}
+                    onChange={handleChange}
+                    required
+                />
+                </label>
+                <label>
+                    Author
+                <input
+                    name="author_input"
+                    type="text"
+                    value={values.book.author}
+                    onChange={handleChange}
+                    required
+                />
+                </label>
+                <label>
+                    Number of Pages
+                <input
+                    name="pages_input"
+                    type="number"
+                    value={values.book.pages}
+                    onChange={handleChange}
+                    required
+                />
+                </label>
+                <button type='submit' value="Submit">
+                    ADD
+                </button>
+            </form>
+        </div>
+    )
+}
