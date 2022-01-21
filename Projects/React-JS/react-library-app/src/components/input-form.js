e532934c1ce1579733b9c79db94c6b24afdb2b5a
Tim David Harris <timdavidharris@gmail.com>
2022-01-20 21:15:53 -0700

    Set up with state

--- Projects/React-JS/react-library-app/src/components/input-form.js
+++ Projects/React-JS/react-library-app/src/components/input-form.js
@@ -1,66 +1,88 @@
-import React, { useState } from 'react';
-// import BookCard from './book-tiles';
+import React, { Component } from 'react';
+import BookCard from './book-tiles';
 import uniqid from 'uniqid';
 
-const initialValues = {
-    book: {
-        title: '',
-        author: '',
-        pages: '',
-        id: uniqid()
-    },
-    library: [],
-}
-
-export default function Form() {
-    const [values, setValues] = useState(initialValues);
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setValues({
-            ...values,
-            [name]: value,
-        })
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            book: {
+                title: '',
+                author: '',
+                pages: '',
+                id: uniqid(),
+            },
+            library: []
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    return (
-        <div id='input-form-div'>
-            {/* <BookCard libraryArray={library}/> */}
-            <form>
-                <label>
-                    Book Title
-                <input 
-                    name="title_input"
-                    type="text"
-                    value={values.book.title}
-                    onChange={handleChange}
-                    required
-                />
-                </label>
-                <label>
-                    Author
-                <input
-                    name="author_input"
-                    type="text"
-                    value={values.book.author}
-                    onChange={handleChange}
-                    required
-                />
-                </label>
-                <label>
-                    Number of Pages
-                <input
-                    name="pages_input"
-                    type="number"
-                    value={values.book.pages}
-                    onChange={handleChange}
-                    required
-                />
-                </label>
-                <button type='submit' value="Submit">
-                    ADD
-                </button>
-            </form>
-        </div>
-    )
+    handleChange(e) {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(e) {
+        this.setState({
+            library: this.state.library.concat(this.state.book),
+            book: {
+                title: '',
+                author: '',
+                pages: '',
+                id: uniqid(),
+            }
+        });
+        e.preventDefault();
+    }
+
+    render() {
+        const {library} = this.state;
+        return (
+            <div id='input-form-div'>
+                <BookCard libraryArray={library}/>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Book Title
+                    <input 
+                        name="book.title"
+                        type="text"
+                        value={this.title}
+                        onChange={this.handleChange}
+                        required
+                    />
+                    </label>
+                    <label>
+                        Author
+                    <input
+                        name="book.author"
+                        type="text"
+                        value={this.author}
+                        onChange={this.handleChange}
+                        required
+                    />
+                    </label>
+                    <label>
+                        Number of Pages
+                    <input
+                        name="book.pages"
+                        type="number"
+                        value={this.pages}
+                        onChange={this.handleChange}
+                        required
+                    />
+                    </label>
+                    <button type='submit' value="Submit">
+                        ADD
+                    </button>
+                </form>
+            </div>
+        )
+    }
 }
+
+export default Form;
