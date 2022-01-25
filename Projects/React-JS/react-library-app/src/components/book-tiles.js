09a97ee651528a20c44b69ebceb28c1aace8d2bb
Tim David Harris <timdavidharris@gmail.com>
2022-01-24 21:00:13 -0700

    Added fn to change read status

--- Projects/React-JS/react-library-app/src/components/book-tiles.js
+++ Projects/React-JS/react-library-app/src/components/book-tiles.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
 const BookCard = (props) => {
-    const libraryArray = props.libraryArray;
-    console.log(libraryArray);
+    const { libraryArray } = props;
+    function changeReadStatus(e) {
+        let key = e.target.dataset.key
+        function theKey(book) {
+            return book.id === key;
+        }
+        let book = libraryArray.find(theKey);
+        if (book.bookRead === "true") {
+            book.bookRead = "false";
+        } else {
+            book.bookRead = "true"
+        }
+    }
     return(
         <div>
             <section>
                 {libraryArray.map((book) => {
-                    return <div key={book.id}>{`${book.title} by ${book.author} has ${book.pages} pages and this book is ${book.bookRead === "true" ? "read" : "unread"}.`} </div>
+                    return <div key={book.id}>{`${book.title} by ${book.author} has ${book.pages} pages and this book is ${book.bookRead === "true" ? "read" : "unread"}.`} <button data-key={book.id} onClick={changeReadStatus}>Change Read Status</button></div>
                 })}
             </section>
         </div>
     )
 }
+
+
 
 export default BookCard;
