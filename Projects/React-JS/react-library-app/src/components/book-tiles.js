79d0acaa1d3331bed462a4e9968f069a276885c0
Tim David Harris <timdavidharris@gmail.com>
2022-01-25 21:05:31 -0700

    Adjusting the button to update read status

--- Projects/React-JS/react-library-app/src/components/book-tiles.js
+++ Projects/React-JS/react-library-app/src/components/book-tiles.js
@@ -3,28 +3,30 @@ import React from 'react';
 const BookCard = (props) => {
     const { libraryArray } = props;
     function changeReadStatus(e) {
+        console.log("ChangeReadStatus")
         let key = e.target.dataset.key
         function theKey(book) {
             return book.id === key;
         }
         let book = libraryArray.find(theKey);
-        if (book.bookRead === "true") {
-            book.bookRead = "false";
+        if (book.bookRead === "read") {
+            book.bookRead = "unread";
         } else {
-            book.bookRead = "true"
+            book.bookRead = "read"
         }
     }
     return(
         <div>
             <section>
                 {libraryArray.map((book) => {
-                    return <div key={book.id}>{`${book.title} by ${book.author} has ${book.pages} pages and this book is ${book.bookRead === "true" ? "read" : "unread"}.`} <button data-key={book.id} onClick={changeReadStatus}>Change Read Status</button></div>
+                    return <div key={book.id}>
+                    {`${book.title} by ${book.author} has ${book.pages} pages and this book is ${book.bookRead}.`} 
+                    <button data-key={book.id} onClick={changeReadStatus}>Change Read Status</button>
+                    </div>
                 })}
             </section>
         </div>
     )
 }
-
-
 
 export default BookCard;
