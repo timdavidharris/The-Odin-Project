4630ea90fe2c3a6b43d940adc06f2f1da1738f3b
Tim David Harris <timdavidharris@gmail.com>
2022-02-02 21:07:32 -0700

    Using hook example from TOP

---
+++ React-JS/hooks-practice/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React, { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={incrementCount}>Increment</button>
+    </div>
+  );
+};
+
+export default App;
