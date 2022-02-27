a42f80bd3189692585ded561c8efa7b19ab73f89
Tim David Harris <timdavidharris@gmail.com>
2022-02-26 18:23:16 -0700

    Basic routing practice, working links

---
+++ React-JS/react-router/src/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return(
+    <div>
+      <h1>Hello from App</h1>
+      <h2>Go to <a href="/profile">Profile</a> </h2>
+    </div>
+  )
+}
+
+export default App;
---
+++ React-JS/react-router/src/Profile.js
@@ -0,0 +1,10 @@
+const Profile = () => {
+    return (
+        <div>
+            <h1>Hello from Profile</h1>
+            <h2>Go to <a href="/">App</a> </h2>
+        </div>
+    )
+}
+
+export default Profile;
